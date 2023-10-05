32d6178ff4bebe08abbc952e0d5e64700f65df63
Rohit-Xieno <r.kumawat@xieno.com>
2023-10-05 15:15:08 +0530

    05-10-2023

--- header.php
+++ header.php
@@ -76,16 +76,7 @@
 							<div class="product-search-form w-[100%]">
 								<form id="product-search-form-0" class="product-search-form show-submit-button flex" action="http://pureayurveda.loc/" method="get">
 									<input id="product-search-field-0" name="s" type="text" class="product-search-field bg-transparent focus:outline-none w-[100%]" placeholder="search your product here..." autocomplete="off">
-									<input type="hidden" name="post_type" value="product">
-									<input type="hidden" name="title" value="1">
-									<input type="hidden" name="excerpt" value="1">
-									<input type="hidden" name="content" value="1">
-									<input type="hidden" name="categories" value="1">
-									<input type="hidden" name="attributes" value="1">
-									<input type="hidden" name="tags" value="1">
-									<input type="hidden" name="sku" value="1">
-									<input type="hidden" name="orderby" value="date-DESC">
-									<input type="hidden" name="ixwps" value="1">
+									<input type="hidden" name="post_type[]" value="product">
 									<span title="Clear" class="product-search-field-clear" style="display:none"></span>
 									<button type="submit" class="ml-auto"><img src="<?php echo get_template_directory_uri() . '/assets/images/search.png' ?>" alt="" class="ml-auto"></button>
 								</form>
